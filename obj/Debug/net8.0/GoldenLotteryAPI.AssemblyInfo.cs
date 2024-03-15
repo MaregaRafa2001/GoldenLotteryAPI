7f1f3f3bb4a4de85a38f93d2c99b9f6a2f5a96c4
Rafael Marega <Rafaelmarega1@gmail.com>
2024-03-15 00:52:12 -0300

    teste

--- obj/Debug/net8.0/GoldenLotteryAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/GoldenLotteryAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoldenLotteryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccd273bead9d151d83e7ecb0bcc996ea3d2e31c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoldenLotteryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoldenLotteryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
